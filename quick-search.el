b01b165fb042ce9452add50ff59d82a554f950b8
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2024-01-31 11:53:07 -0300

    Added prefix to all the functions
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- quick-search.el
+++ quick-search.el
@@ -28,7 +28,7 @@
 ;; Modes part
 
 ;;; Taken from: https://stackoverflow.com/a/1511827
-(defun which-active-modes ()
+(defun quick-search/which-active-modes ()
   "Returns all the modes (both major and minor) that are enabled in the current buffer."
   (let ((active-modes))
     ;;Adds all minor modes to the active-modes variable
@@ -47,78 +47,73 @@
 
 ;; Search engine part
 
-(defvar *search-engines* nil)
-(setq *search-engines* nil)
+(defvar *quick-search/search-engines* nil)
+(setq *quick-search/search-engines* nil)
 
-(defun add-search-engine (name search-query modes)
+(defun quick-search/add-search-engine (name search-query modes)
   "Creates a 'Search engine object' that the quick-search function will take into account"
   (let (
         (new-engine (list name search-query modes))
         )
-    (add-to-list '*search-engines* new-engine)
+    (add-to-list '*quick-search/search-engines* new-engine)
     ))
 
-(defun get-engine-name (engine)
+(defun quick-search/get-engine-name (engine)
   (nth 0 engine))
 
-(defun get-engine-search (engine)
+(defun quick-search/get-engine-search (engine)
   (nth 1 engine))
 
-(defun get-engine-modes (engine)
+(defun quick-search/get-engine-modes (engine)
   (nth 2 engine))
 
-(defun check-if-engine-is-valid (engine modes)
+(defun quick-search/check-if-engine-is-valid (engine modes)
   (let (
-        (engine-modes (get-engine-modes engine))
+        (engine-modes (quick-search/get-engine-modes engine))
         )
     (if (not engine-modes) 
         't ;;If the engine has "nil" as modes, then that means it has to be turned on globally
       (seq-intersection engine-modes modes)
     )))
 
-(defun get-valid-engines (engines modes)
+(defun quick-search/get-valid-engines (engines modes)
   (seq-filter (lambda (engine)
-	  (check-if-engine-is-valid engine modes))
+	  (quick-search/check-if-engine-is-valid engine modes))
 	engines)
   )
 
-(defun get-engine-from-name (engine-name engines)
+(defun quick-search/get-engine-from-name (engine-name engines)
   "Get the 'Engine Object', given the name"
-    (seq-find (lambda (engine) (equal engine-name (get-engine-name engine))) engines)
+    (seq-find (lambda (engine) (equal engine-name (quick-search/get-engine-name engine))) engines)
     )
 
 
-
-
-
-
-
 ;; Browser part
-(defvar *prefered-browser* nil)
-(setq *prefered-browser** nil)
+(defvar *quick-search/prefered-browser* nil)
+(setq *quick-search/prefered-browser* nil)
 
-(defun set-prefered-browser (name arguments)
+(defun quick-search/set-prefered-browser (name arguments)
   "Sets the prefered-browser variable. This variable is used to call the shell command that will open the browser itself"
   (setq *prefered-browser* (cons name arguments)))
 
-(defun get-browser-name (browser)
+(defun quick-search/get-browser-name (browser)
   "Sets the prefered-browser variable. This variable is used to call the shell command that will open the browser itself"
   (car *prefered-browser*))
 
-(defun get-browser-argument (browser)
+(defun quick-search/get-browser-argument (browser)
   "Sets the prefered-browser variable. This variable is used to call the shell command that will open the browser itself"
   (cdr *prefered-browser*))
 
-(defun search-on-browser (engine what)
+(defun quick-search/search-on-browser (engine what)
   "Search TERM on preferred engine on engine.
 
 If no search TERM is entered interactively, the current
 buffer selection is used as the TERM."
   (let*
       (
-       (browser-name (get-browser-name *prefered-browser*))
-       (browser-argument (get-browser-argument *prefered-browser*))
-       (engine-search (get-engine-search engine))
+       (browser-name (quick-search/get-browser-name *prefered-browser*))
+       (browser-argument (quick-search/get-browser-argument *prefered-browser*))
+       (engine-search (quick-search/get-engine-search engine))
        (full_query_url (concat engine-search "'" what "'"))
        )
     (shell-command (concat browser-name " " browser-argument full_query_url) nil nil)
@@ -130,20 +125,19 @@ buffer selection is used as the TERM."
   (interactive
    (let* (
          (completion-ignore-case  t)
-         (valid-engines (get-valid-engines *search-engines* (which-active-modes)))
+         (valid-engines (quick-search/get-valid-engines *quick-search/search-engines* (quick-search/which-active-modes)))
          (engine-names (mapcar (lambda (engine)
-			    (get-engine-name engine))
+			    (quick-search/get-engine-name engine))
 			 valid-engines))
          )
      (list
-      (get-engine-from-name
+      (quick-search/get-engine-from-name
        (completing-read "Which search engine: " engine-names nil t) valid-engines)
       (read-string "What: ")
       )
      ))
-  (search-on-browser where what)
+  (quick-search/search-on-browser where what)
   )
-
 
 (provide 'quick-search)
 ;;; quick-search.el ends here
